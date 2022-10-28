8f7388864d0035c849505e47203979306da99133
UroobaShameem <uroobashameem12@gmail.com>
2022-10-28 15:09:41 +0500

    updated

---
+++ asset/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelectorAll('.dropdown-menu').forEach(function(element){
+		element.addEventListener('click', function (e) {
+		  e.stopPropagation();
+		});
+	})
+}); 
